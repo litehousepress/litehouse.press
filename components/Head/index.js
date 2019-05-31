5c2793c12082658270ac301ac8f6f87e96b28e59
Jan Radl <jan.radl@citrix.com>
2019-05-31 21:20:47 +0200

    New homepage

--- components/Head/index.js
+++ components/Head/index.js
@@ -5,16 +5,16 @@ export default () => (
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
         <title>Litehouse</title>
-        <meta name="description" content="Bringing the new frontiers of journalistm. " />
+        <meta name="description" content="New Frontier of Online Journalism" />
         <meta property="og:title" content="Jetcheater" />
-        <meta property="og:description" content="Bringing the new frontiers of journalistm. " />
+        <meta property="og:description" content="New Frontier of Online Journalism" />
         <meta name="og:url" content="https://www.litehouse.press" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/static/favicons/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
-        <link rel="manifest" href="/static/favicons/site.webmanifest" />
-        <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#ffffff" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/static/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/static/favicon/site.webmanifest" />
+        <link rel="mask-icon" href="/static/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
         <meta
             name="google-site-verification"
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Page from '../components/Page';
 import Head from '../components/Head';
+import Header from '../components/Header';
 import Hero from '../components/Hero';
 import './styles.scss';
 
@@ -7,6 +8,7 @@ function Home() {
     return (
         <Page>
             <Head />
+            <Header />
             <Hero />
         </Page>
     );
